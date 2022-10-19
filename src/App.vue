3425c7fc7c5ce192d77b6e8962189824185dd486
Hevel (Axel) <42890916+HevelMc@users.noreply.github.com>
2022-10-19 11:31:15 +0200

    Added router view for future multi pages

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
 import Footer from './components/Footer.vue';
 import Header from './components/Header.vue';
-import Home from './components/Home.vue';
 </script>
 
 <template>
   <div class="flex flex-col h-screen">
     <Header />
     <div id="content" class="flex-grow">
-      <Home />
+      <router-view />
     </div>
     <Footer />
   </div>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue';
-import './style.css';
+import { createWebHistory, createRouter } from "vue-router";
+import Home from './components/Home.vue';
+import Test from './components/Test.vue';
 import App from './App.vue';
+import './style.css';
 
-createApp(App).mount('#app');
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount('#app');
